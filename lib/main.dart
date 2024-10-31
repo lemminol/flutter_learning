511d438545f50a8b808adff9a367ba3646162b7c
lemminol <30825512+lemminol@users.noreply.github.com>
2024-10-31 16:48:59 +0900

    learning

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Charactor Card',
+    home: MyCard(),
+
+
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BBANTO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello')
+          ],
+        ),
+      ),
+
+
+
+    );
+  }
+}
